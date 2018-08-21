5a42c3f327fc73e31bf03dece2aee1fb4a3e8320
dlannan <dlannan@internode.on.net>
2018-08-21 22:21:50 +0930

    Axles attached to body.
    Attached two rear axle stubs to the main body.

--- src/projects/test-urdf/test2.lua
+++ src/projects/test-urdf/test2.lua
@@ -11,7 +11,7 @@ local timer = require('timer')
 local simApp = {}
 
 
-function simApp:createWheel( x, y, z, r, w, cid, vid )
+function simApp:createWheel( x, y, z, r, w, mass)
 
     local radius    = r
     local width     = w
@@ -29,7 +29,7 @@ function simApp:createWheel( x, y, z, r, w, cid, vid )
     --     gbullet.b3CreateCollisionShapeSetChildTransform( cmd, cid, pos, rot )
     -- end
     local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)
-    cid = gbullet.b3GetStatusCollisionShapeUniqueId(status)
+    local cid = gbullet.b3GetStatusCollisionShapeUniqueId(status)
 
     local cmd = gbullet.b3CreateVisualShapeCommandInit( self.client )
     local vindex = gbullet.b3CreateVisualShapeAddCylinder( cmd, radius, width )
@@ -37,13 +37,13 @@ function simApp:createWheel( x, y, z, r, w, cid, vid )
     --     gbullet.b3CreateVisualShapeSetChildTransform( cmd, vid, pos, rot )
     -- end
     local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)
-    vid = gbullet.b3GetStatusVisualShapeUniqueId(status)
+    local vid = gbullet.b3GetStatusVisualShapeUniqueId(status)
     gbullet.b3CreateVisualShapeSetRGBAColor( cmd, vid, col )
 
     p("Visual Object Created:", gbullet.b3GetStatusType(status) == gbullet.CMD_CREATE_VISUAL_SHAPE_COMPLETED)
     
     local cmd = gbullet.b3CreateMultiBodyCommandInit(self.client)
-    local rigidbody = gbullet.b3CreateMultiBodyBase( cmd, 2.0, cid, vid, pos, rotflip, frameP, frameR )
+    local rigidbody = gbullet.b3CreateMultiBodyBase( cmd, mass, cid, vid, pos, rotflip, frameP, frameR )
     local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)
     
     local bidx = gbullet.b3GetStatusBodyIndex( status )
@@ -54,24 +54,24 @@ end
 
 ------------------------------------------------------------------------------------------------------------
 
-function simApp:createWheelJoint( pb, cb, px, py, pz, cx, cy, cz )
+function simApp:createFixedWheelJoint( pb, cb, px, py, pz, cx, cy, cz )
 
     local pos = ffi.new( "double[3]", { [0]=0.0, 0.0, 0.0 } )
     local rot = ffi.new( "double[4]", { [0]=0.0, 0.0, 0.0, 1.0} )
     local fchild = ffi.new( "double[7]", { [0]=cx, cy, cz, 0.0, 0.0, 0.0, 1.0 } )
     local fparent = ffi.new( "double[7]", { [0]=px, py, pz, 0.0, 0.0, 0.0, 1.0 } )
-    local axis = ffi.new( "double[3]", { [0]=0.0, 1.0, 0.0} )
+    local axis = ffi.new( "double[3]", { [0]=0.0, 0.0, 1.0} )
 
     local joint = ffi.new("struct b3JointInfo[1]")
-    joint[0].m_jointType = gbullet.eFixedType
+    joint[0].m_jointType = gbullet.ePoint2PointType
     joint[0].m_jointAxis = axis
     joint[0].m_jointName = "joint"..cb
     joint[0].m_linkName = "link"..cb
-    joint[0].m_parentFrame = fchild
-    joint[0].m_childFrame = fparent
+    joint[0].m_parentFrame = fparent
+    joint[0].m_childFrame = fchild
     joint[0].m_parentIndex = pb
-    joint[0].m_jointLowerLimit = 0.1 
-    joint[0].m_jointUpperLimit = 0.1 
+    joint[0].m_jointLowerLimit = 0.01 
+    joint[0].m_jointUpperLimit = 0.02 
 
     local cmd = gbullet.b3InitCreateUserConstraintCommand( self.client, pb, -1, cb, -1, joint )
     local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)
@@ -79,6 +79,39 @@ function simApp:createWheelJoint( pb, cb, px, py, pz, cx, cy, cz )
     return bl 
 end
 
+------------------------------------------------------------------------------------------------------------
+
+function simApp:createBody( x, y, z, w, h, l, mass )
+
+    local col = ffi.new( "double[4]", { [0]=0.0, 1.0, 0.0, 1.0} )
+    local extents = ffi.new( "double[3]", {[0]=w*0.5, h*0.5, l*0.5} )
+    local pos = ffi.new( "double[3]", { [0]=x, y, z } )
+    local frameP = ffi.new( "double[3]", { [0]=0.0, 0.0, 0.0 } )
+    local rot = ffi.new( "double[4]", { [0]=0.0, 0.0, 0.0, 1.0} )
+    local frameR = ffi.new( "double[4]", { [0]=0.0, 0.0, 0.0, 1.0 } )
+
+    local cmd = gbullet.b3CreateCollisionShapeCommandInit( self.client )
+    local cindex = gbullet.b3CreateCollisionShapeAddBox( cmd, extents )
+    local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)
+    local cid = gbullet.b3GetStatusCollisionShapeUniqueId(status)
+
+    local cmd = gbullet.b3CreateVisualShapeCommandInit( self.client )
+    local vindex = gbullet.b3CreateVisualShapeAddBox( cmd, extents )
+    local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)
+    local vid = gbullet.b3GetStatusVisualShapeUniqueId(status)
+    gbullet.b3CreateVisualShapeSetRGBAColor( cmd, vid, col )
+
+    p("Visual Object Created:", gbullet.b3GetStatusType(status) == gbullet.CMD_CREATE_VISUAL_SHAPE_COMPLETED)
+    
+    local cmd = gbullet.b3CreateMultiBodyCommandInit(self.client)
+    local rigidbody = gbullet.b3CreateMultiBodyBase( cmd, mass, cid, vid, pos, rot, frameP, frameR )
+    local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)
+    
+    local bidx = gbullet.b3GetStatusBodyIndex( status )
+    p("Multibody Created:", gbullet.b3GetStatusType(status) == gbullet.CMD_CREATE_MULTI_BODY_COMPLETED )
+    
+    return bidx, cid, vid
+end
 ------------------------------------------------------------------------------------------------------------
 -- simStartup - general initialisation
 
@@ -135,17 +168,24 @@ function simApp:Startup()
     vehicle.wheels = {}
 
     --gbullet.b3LoadUrdfCommandSetStartPosition(self.car.physics, 0.0, 10.0, 0.0)
-    vehicle.wheels.FL = self:createWheel(2.0, 1.0, 1.0, 0.5, 0.25)
-    vehicle.wheels.FR = self:createWheel(2.0, -1.0, 1.0, 0.5, 0.25)   
+    vehicle.wheels.FL = self:createWheel(2.0, 1.0, 1.0, 0.5, 0.25, 2.0)
+    vehicle.wheels.FR = self:createWheel(2.0, -1.0, 1.0, 0.5, 0.25, 2.0)   
 
-    -- rear axle
-    local axb, axc, axv = self:createWheel(-2.0, 0.0, 1.0, 0.1, 1.5)   
+    -- rear axle split
+    local laxb, laxc, laxv = self:createWheel(-2.0, 0.2, 1.0, 0.1, 0.1, 2.0)   
+    local raxb, raxc, raxv = self:createWheel(-2.0, -0.2, 1.0, 0.1, 0.1, 2.0)  
 
-    local rlb, rlc, rlv = self:createWheel(-2.0, 1.0, 1.0, 0.5, 0.25)    
-    local rrb, rrc, rrv = self:createWheel(-2.0, -1.0, 1.0, 0.5, 0.25)   
+    local rlb, rlc, rlv = self:createWheel(-2.0, 1.0, 1.0, 0.5, 0.25, 2.0)    
+    local rrb, rrc, rrv = self:createWheel(-2.0, -1.0, 1.0, 0.5, 0.25, 2.0)   
 
-    self:createWheelJoint( axb, rlb, 0.0, 0.0, 0.75, 0.0, 0.0, -0.25 )
-    self:createWheelJoint( axb, rrb, 0.0, 0.0, -0.75, 0.0, 0.0, 0.25 )
+    self:createFixedWheelJoint( laxb, rlb, 0.0, 0.0, -0.05, 0.0, 0.0, 0.125 )
+    self:createFixedWheelJoint( raxb, rrb, 0.0, 0.0, 0.05, 0.0, 0.0, -0.125 )
+
+    local bb, bc, bv = self:createBody( 0.0, 0.0, 1.0, 4.5, 1.2, 0.7, 20 )
+
+    self:createFixedWheelJoint( bb, laxb, -2.0, 0.6, 0.0, 0.0, 0.0, 0.05 )
+    self:createFixedWheelJoint( bb, raxb, -2.0, -0.6, 0.0, 0.0, 0.0, -0.05 )
+
     self.simInit = 0
 
     ------------------------------------------------------------------------------------------------------------
@@ -167,9 +207,6 @@ function simApp:Update( )
             gbullet.b3PhysicsParamSetRealTimeSimulation(cmd, 1)
             gbullet.b3PhysicsParamSetNumSubSteps(cmd, 6)
             local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)        
-
-            local cmd = gbullet.b3InitRequestDebugLinesCommand(self.client, 0x182c)
-            local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)        
         end
     end
 
@@ -184,6 +221,10 @@ function simApp:Update( )
         local cmd = gbullet.b3InitStepSimulationCommand(self.client)
         local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)
     end
+
+    --local cmd = gbullet.b3InitRequestDebugLinesCommand(self.client, 1) --0x182c)
+    --local status = gbullet.b3SubmitClientCommandAndWaitStatus(self.client, cmd)        
+    --p("Debug Visuals: ", gbullet.b3GetStatusType(status) == gbullet.CMD_DEBUG_LINES_COMPLETED)            
 
     -- Something bad has happend - weve lost client connect etc
     --local cmd = gbullet.b3ProcessServerStatus(self.client)
